35bbacfd567f018a4a49307a53869f7d199317c7
Chris Wu <qiangwu6@gmail.com>
2025-10-30 13:35:49 -0400

    started the edit-form page

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 ### Create outfits from uploaded pictures and store them with own personal rating. Friends can also view and rate your outfits themselves, which gives you your trending rating graph
 ### Figure out AI Personal outfit creator for OOTD
 ### Figure out social connections
-### Figure out how to display user's own wardrobe: spinning clothes or swiping l/r through or scroll u/d through available clothes
+### Figure out how to display user's own wardrobe: spinning clothes and swiping l/r
 ### Complete the entire dashboard
 ### Complete the ui outfits forms, and table
 ## For the outfits forms, for MVP don't focus on camera + ML clothes detection and extraction, just upload image.
--- app/ui/outfits/edit-form.tsx
+++ app/ui/outfits/edit-form.tsx
@@ -1,7 +1,29 @@
 "use client";
 
-export default function EditOutfitForm({}) {
+import Link from 'next/link';
+import {Outfit} from '@/app/lib/definitions'
+import {Button} from '@/app/ui/button';
+import {updateOutfit, OutfitState} from '@/app/lib/actions';
+import {useActionState} from 'react';
+
+export default function EditOutfitForm({outfit}: {outfit: Outfit}) {
+    const initialState: OutfitState = {message: null, errors: {}};
+    const updateOutfitWithId = updateOutfit.bind(null, outfit.id);
+    const [state, formAction] = useActionState(updateOutfitWithId, initialState);
+
     return (
-        <div></div>
+        <form action = {formAction}>
+            <div className = "rounded-md bg-gray-50 p-4 md:p-6">
+                <div className = "mb-4">
+                    <p className = "text-md">
+                        Editing {outfit.name}
+                    </p>
+                </div>
+
+                <div className = "mb-4">
+                    
+                </div>
+            </div>
+        </form>
     );
 }
